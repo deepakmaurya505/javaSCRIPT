66ee239afd71fd6b650009eabba6d27f6da58621
Deepak Maurya <mauryaji2404@gmail.com>
2025-08-11 22:25:42 +0530

    classes

--- 10_OOPS/tempCodeRunnerFile.js
+++ 10_OOPS/tempCodeRunnerFile.js
@@ -1,3 +1,19 @@
 
-// myheroes.deepak()
-// myheroes.heyDeepak()
+// class User {
+//     constructor(username, email, password){
+//         this.username = username;
+//         this.email = email;
+//         this .password = password;
+//     }
+//     encryptPassword(){
+//     return `${this.password}abc`
+//     }
+//     changeusername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// const chai = new User("chai", "chai@gmail.com", "123")
+
+// console.log(chai.encryptPassword());
+// console.log(chai.changeusername());
